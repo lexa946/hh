2872357aca18b8c8a965116110e763a000c8d806
Алексей Пожар <a.pozhar@x5.ru>
2024-08-17 15:45:40 +0300

    Remove creds from main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from hh import HHParser
 
+from creds import HH_LOGIN, HH_PASSWORD
+
 host = "https://nn.hh.ru"
 
 hh_parser = HHParser(host)
 
-hh_parser.login("pozhara98@mail.ru", "89108787515z")
+hh_parser.login(HH_LOGIN, HH_PASSWORD)
 
 my_resumes = hh_parser.get_resumes_small()
 print()
