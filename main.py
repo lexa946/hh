88f790d65fa95e85be5fd652fee081e804db1dcc
Алексей Пожар <a.pozhar@x5.ru>
2024-08-18 14:44:47 +0300

    поправил бота отправки откликов и добавил логирование

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
+import time
 from HH import HHParser
-
 from creds import HH_LOGIN, HH_PASSWORD
+from loguru import logger
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+
+logger.add("response.log")
 host = "https://hh.ru"
 
 hh_parser = HHParser(host)
-
 hh_parser.login(HH_LOGIN, HH_PASSWORD)
-
 my_resumes = hh_parser.get_resumes_small()
-
 developer_resume = my_resumes[0]
-resume_large = hh_parser.get_resume_large(developer_resume.id)
-
 
 for vacancies in hh_parser.get_vacancies_small("Python Developer", only_in_title=True, from_IT_company=True):
     for vacancy in vacancies:
         hh_parser.send_response_to_vacancy(vacancy, developer_resume)
-    print()
-print()
-
+        logger.info(f"Отправил отклик на вакансию {vacancy}. Ссылка на вакансию: {vacancy.link_more_info}.")
+        time.sleep(10)
